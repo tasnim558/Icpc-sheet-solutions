fb8b5aba135fb1b86d405cba2b9d631b48065b1c
tasnim558 <tasnimtaaha78@gmail.com>
2026-01-14 20:50:08 +0600

    Create Problem_E.c

---
+++ Sheet #5 (Functions)/Problem_E.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// swap function
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+
+    swap(&x, &y);  
+
+    printf("%d %d", x, y);
+
+    return 0;
+}

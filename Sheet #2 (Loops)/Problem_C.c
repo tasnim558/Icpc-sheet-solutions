9aa3541e6d24a04bac03cf31a26196ef744950e8
tasnim558 <tasnimtaaha78@gmail.com>
2026-01-14 19:53:05 +0600

    Create Problem_C.c

---
+++ Sheet #2 (Loops)/Problem_C.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, num;
+    int even = 0, odd = 0, positive = 0, negative = 0;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &num);
+
+        if(num % 2 == 0)
+            even++;
+        else
+            odd++;
+
+        if(num > 0)
+            positive++;
+        else if(num < 0)
+            negative++;
+    }
+
+    printf("Even: %d\n", even);
+    printf("Odd: %d\n", odd);
+    printf("Positive: %d\n", positive);
+    printf("Negative: %d\n", negative);
+
+    return 0;
+}

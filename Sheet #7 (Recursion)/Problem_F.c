6e56862bdabec5a982d4a9e62f86e96e9018c886
tasnim558 <tasnimtaaha78@gmail.com>
2026-01-14 21:11:15 +0600

    Update Problem_F.c

--- Sheet #7 (Recursion)/Problem_F.c
+++ Sheet #7 (Recursion)/Problem_F.c
@@ -1,18 +1,54 @@
-#include <stdio.h>
+a) #include <stdio.h>
 
-void factorial(int i, int n, long long multi)
+void printEven(long long a[], int i)
 {
-    if (i > n) {
-        printf("%lld\n", multi);
+    if (i < 0)
         return;
-    }
-    factorial(i + 1, n, multi * i);
+
+    if (i % 2 == 0)
+        printf("%lld ", a[i]);   // (bottom-up)
+
+    printEven(a, i - 1); 
 }
 
-int main()//bottom up method 
+int main()
 {
     int n;
     scanf("%d", &n);
-    factorial(1, n, 1);
+
+    long long a[n];
+    for (int i = 0; i < n; i++)
+        scanf("%lld", &a[i]);
+
+    printEven(a, n - 1);
+
+    return 0;
+}
+
+
+b) #include <stdio.h>
+
+void printEven(long long a[], int n, int i)
+{
+    if (i >= n)
+        return;
+
+    printEven(a, n, i + 1);   // (top-down)
+
+    if (i % 2 == 0)                 
+        printf("%lld ", a[i]);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    long long a[n];
+    for (int i = 0; i < n; i++)
+        scanf("%lld", &a[i]);
+
+    printEven(a, n, 0);
+
     return 0;
 }
